6cd65a83fa0d994945bd78769b75a18d3e630d09
anhur <a.khamees@student.alfa-college.nl>
2018-06-22 03:53:23 +0200

    -- Renamed almost everything
    ---- Implemented addItems, ShowMyItems and Logout function
    ------ Fixed a lot of code issues

--- src/BUS/Main.java
+++ src/BUS/Main.java
@@ -1,7 +1,6 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,21 +8,17 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Auth auth = new Auth();
-    Controller controller;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setTitle("Login BUS");
-        primaryStage.setScene(new Scene(root, 300, 475));
+        primaryStage.setScene(new Scene(root, 500, 475));
         primaryStage.show();
+
     }
 
     public static void main(String[] args) throws Exception {
-
         launch(args);
-
         DB.getConnection().close();
     }
 }
---
+++ src/BUS/MainController.java
@@ -0,0 +1,108 @@
+package BUS;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Auth {
+
+    @FXML
+    private TextField _email;
+
+    @FXML
+    Label pTitle;
+
+    @FXML
+    private PasswordField _pass;
+
+    @FXML
+    private Label lTitle;
+
+    @FXML
+    private GridPane LoginPane;
+
+    @FXML
+    private AnchorPane ProfilePane;
+
+    @FXML
+    private Button addItemB;
+
+
+    private String email;
+
+    private String pass;
+
+    public boolean Login() throws Exception{
+
+        String SQL = "SELECT email, password FROM users WHERE email = '" + _email.getText() + "' && password='" + _pass.getText() + "'";
+
+        Statement Cstatement = DB.getConnection().createStatement();
+
+        ResultSet rs = Cstatement.executeQuery(SQL);
+
+        while (rs.next()){
+            email = rs.getString("email");
+            pass = rs.getString("password");
+        }
+
+        if(_email.getText().equals(email) && _pass.getText().equals(pass)) {
+            Stage stage = (Stage) LoginPane.getScene().getWindow();
+            stage.close();
+
+            Stage primaryStage2 = new Stage();
+
+            Parent root = FXMLLoader.load(getClass().getResource("profile.fxml"));
+            primaryStage2.setTitle("Profile BUS");
+            primaryStage2.setScene(new Scene(root, 400, 475));
+            primaryStage2.show();
+
+            return true;
+
+        } else {
+            lTitle.setText("login Failed");
+        }
+        return false;
+    }
+
+    @FXML
+    private boolean logout(ActionEvent event) throws Exception{
+        Stage stageOut = (Stage) ProfilePane.getScene().getWindow();
+        stageOut.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login BUS");
+        primaryStage.setScene(new Scene(root, 500, 475));
+        primaryStage.show();
+        return true;
+    }
+
+    @FXML
+    private void addItem(ActionEvent event) throws Exception{
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("items.fxml"));
+        primaryStage.setTitle("Items Toevoegen");
+        primaryStage.setScene(new Scene(root, 600, 475));
+        primaryStage.show();
+    }
+
+    @FXML
+    private void myItems(ActionEvent event) throws Exception{
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("myItems.fxml"));
+        primaryStage.setTitle("Mijn Items");
+        primaryStage.setScene(new Scene(root, 600, 475));
+        primaryStage.show();
+    }
+}
